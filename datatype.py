8a51dfd60bbe29fced53db1fc8a48a9b641e9a6f
unknown <wangarraakoth@gmail.com>
2022-08-30 17:46:35 +0300

    pythonPrac

---
+++ datatype.py
@@ -0,0 +1,27 @@
+#Specifying DataTypes in  Python
+
+y="Shabby"
+
+print(type(y))
+
+
+z=30.567
+print(type(z))
+
+# function that returns a boolean value
+
+def myFunc():
+    return True
+print(myFunc())
+
+# A function that returns "YES" if the value of the boolean is True otherwise print "NO" if the fucton is false
+
+def myFunction():
+    return True
+
+if myFunction():
+    print("YES")
+else:
+    print("NO")
+
+
